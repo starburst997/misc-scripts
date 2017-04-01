65f4ebe3374806984f1631536b4198f795154438
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-04-01 15:13:18 -0400

    Added explanation to script

--- pulls.sh
+++ pulls.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Loop all directories inside the current directory and call 'git pull'
+
 for dir in */
 do
-    dir=${dir%*/}
-    cd ${dir##*/}
+  dir=${dir%*/}
+  cd ${dir##*/}
 
-    echo Pull ${dir##*/}
-    git pull
+  echo Pull ${dir##*/}
+  git pull
 
-    cd ..
+  cd ..
 done
